ac1eb6ff8980166bbe54510ad750c461787ae6a8
Z-i-n-k-s <zishancodes1@gmail.com>
2025-01-12 17:37:16 +0600

    testing with a normal version of backend

--- api-backend/routes/api.php
+++ api-backend/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\TestController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-//Route::get('/test', [TestController::class, 'getTestHuman']);
-Route::get('/test/{id}', [TestController::class, 'getTestHumanWithId']);
-Route::get('/test', [TestController::class, 'getTestHuman'])->middleware('test.middleware');
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
